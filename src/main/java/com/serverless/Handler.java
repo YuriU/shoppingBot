e24e0d7c35670952dea78423c9d4cfdd76f90f19
Yurii Ulianets <yuriiu@amazon.com>
2023-01-04 22:31:09 -0800

    Added references to Telegram Bot API

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -4,22 +4,33 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-
+import java.io.InputStream;
+import java.io.OutputStream;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.StandardCharsets;
+
+public class Handler implements RequestStreamHandler {
 
 	private static final Logger LOG = Logger.getLogger(Handler.class);
 
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-				.build();
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws java.io.IOException {
+		Update update;
+		try {
+			String result = IOUtils.toString(input, StandardCharsets.UTF_8);
+			System.out.println(result);
+			update = MAPPER.readValue(result, Update.class);
+		} catch (Exception e) {
+			System.err.println("Failed to parse update: " + e);
+			throw new RuntimeException("Failed to parse update!", e);
+		}
 	}
 }
