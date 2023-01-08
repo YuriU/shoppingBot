6b92cfd26d6ffe41efaf002ae101d96b1134f3b9
Yurii Ulianets <yuriiu@amazon.com>
2023-01-07 23:12:39 -0800

    Added message handler

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,36 +1,43 @@
 package com.serverless;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 import java.util.Collections;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-import java.io.InputStream;
-import java.io.OutputStream;
-import com.amazonaws.services.lambda.runtime.Context;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.io.IOUtils;
-import java.nio.charset.StandardCharsets;
-
-public class Handler implements RequestStreamHandler {
-
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 	private static final Logger LOG = Logger.getLogger(Handler.class);
 
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 
+	private static final ShoppingBot BOT = new ShoppingBot(
+			"<TOKEN>",
+			"Bot",
+			"Path"
+	);
+
 	@Override
-	public void handleRequest(InputStream input, OutputStream output, Context context) throws java.io.IOException {
-		Update update;
-		try {
-			String result = IOUtils.toString(input, StandardCharsets.UTF_8);
-			System.out.println(result);
-			update = MAPPER.readValue(result, Update.class);
-		} catch (Exception e) {
-			System.err.println("Failed to parse update: " + e);
-			throw new RuntimeException("Failed to parse update!", e);
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		if (input.containsKey("body")) {
+			LOG.info("receivedType: " + input.get("body").getClass());
+			LOG.info("received: " + input.get("body"));
+			try {
+				Update update = MAPPER.readValue((String) input.get("body"), org.telegram.telegrambots.meta.api.objects.Update.class);
+				BOT.onWebhookUpdateReceived(update);
+			} catch (com.fasterxml.jackson.core.JsonProcessingException e) {
+				throw new RuntimeException(e);
+			}
 		}
+
+		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
 	}
 }
---
+++ src/main/java/com/serverless/ShoppingBot.java
@@ -0,0 +1,52 @@
+package com.serverless;
+
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ShoppingBot extends TelegramWebhookBot {
+    private static final Logger LOG = Logger.getLogger(ShoppingBot.class);
+    private final String token;
+    private final String userName;
+    private final String botPath;
+
+    public ShoppingBot(String token, String userName, String botPath) {
+        this.token = token;
+        this.userName = userName;
+        this.botPath = botPath;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public String getBotPath() {
+        return botPath;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        LOG.info("Update received");
+        try {
+            execute(SendMessage
+                    .builder()
+                    .chatId(update.getMessage().getChatId())
+                    .text("Putin Huilo!")
+                    .build());
+
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
